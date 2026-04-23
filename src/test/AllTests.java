fa0c580a18fdb53cb43711cb7933210157b7387d
pcooper <pcooper@horizon.csueastbay.edu>
2026-04-23 11:15:15 -0700

    updated AllTests to use classes instead of packages

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -1,12 +1,15 @@
 package test;
 
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("All Tests")
-@SelectPackages("*test")
+//@SelectPackages("test")
+//@SelectClasses({BlackjackServerTest.class, BlackjackClient.class, MessageStatusTest.class})
+@SelectClasses({MessageStatusTest.class})
 public class AllTests {
 
 }
