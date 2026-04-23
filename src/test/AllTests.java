013504db1843eebc6586d20b58d95652bed7d26b
pcooper <pcooper@horizon.csueastbay.edu>
2026-04-23 11:02:03 -0700

    updated workflow, reverting to --scan-class-path out --details verbose, Added Displayname and wildcard to testsuite

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -2,9 +2,11 @@ package test;
 
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
-@SelectPackages("test")
+@SuiteDisplayName("All Tests")
+@SelectPackages("*test")
 public class AllTests {
 
 }
