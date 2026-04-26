f42fb20d734da61e350279b80d5fc7f4f736b886
pcooper <pcooper@horizon.csueastbay.edu>
2026-04-26 14:05:00 -0700

    adding user and userRole classes, fix minutes

---
+++ src/main/UserRole.java
@@ -0,0 +1,8 @@
+package main;
+
+public enum UserRole {
+	PLAYER, //End user playing the game
+	DEALER, //User managing game play
+	DEVELOPER //Allow user to login as either a PLAYER or a DEALER
+
+}
