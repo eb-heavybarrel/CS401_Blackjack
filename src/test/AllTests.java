9a488edd414fdcb941731e799a666d5ee06f3b8e
pcooper <pcooper@horizon.csueastbay.edu>
2026-04-29 16:26:39 -0700

    using test Classes instead of packages

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -1,14 +1,14 @@
 package test;
 
-//import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
+//import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
-@SuiteDisplayName("All Tests")
-@SelectPackages("test")
-//@SelectClasses({BlackjackServerTest.class, BlackjackClient.class, MessageStatusTest.class})
+//@SuiteDisplayName("All Tests")
+//@SelectPackages("test")
+@SelectClasses({BlackjackServerTest.class, MessageStatusTest.class})
 //@SelectClasses({MessageStatusTest.class})
 public class AllTests {
 
