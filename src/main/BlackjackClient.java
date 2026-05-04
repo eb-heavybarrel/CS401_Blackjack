6f3a0ebc8462a64590fb0a90f832c3305b5263c2
n1ckvu <157045221+n1ckvu@users.noreply.github.com>
2026-05-04 13:35:23 -0700

    Add files via upload

--- src/main/BlackjackClient.java
+++ src/main/BlackjackClient.java
@@ -1,86 +1,80 @@
-package main;
+package BJClient;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class BlackjackClient {
+//	try (var listener = new ServerSocket(59090)) {
+//        System.out.println("Listening on Port 59090");
 
-	public static void main(String[] args) {
-		User user = null;
-		Scanner sc = new Scanner(System.in);
-		String host;
-		int port = 2121;
+	private String IPaddress() {
+		String ip = "no IP address";
 
-		// System.out.println("Enter host IP address: ");
-		// host = sc.nextLine();
-		//host = "192.168.1.101"; // hardcoding IP address while testing
-		host = "localhost"; // hardcoding localhost while testing
-		if (host.equals(""))
-			return;
+		try {
+			Enumeration e = NetworkInterface.getNetworkInterfaces();
+			while (e.hasMoreElements()) {
+				NetworkInterface n = (NetworkInterface) e.nextElement();
 
-		try (Socket socket = new Socket(host, port)) {
-			ObjectOutputStream objOut = new ObjectOutputStream(socket.getOutputStream());
-			objOut.flush();
-			ObjectInputStream objIn = new ObjectInputStream(socket.getInputStream());
+				if (n.isUp() && !n.isLoopback()) {
 
-			// user login/create account
-			while (user == null) {
-				MessageClass mClass = MessageClass.USER;
-				MessageType mType = MessageType.USER_LOGIN;
-
-				List<Object> mData = new ArrayList<>();
-				System.out.println("Enter UserName: ");
-				String userName = sc.nextLine();
-				mData.add(userName);
-
-				System.out.println("Enter Password: ");
-				String password = sc.nextLine();
-				mData.add(password);
-
-				Message loginMessage = new Message(mClass, mType, mData);
-				objOut.writeObject(loginMessage);
-				objOut.flush();
-
-				Message response = (Message) objIn.readObject();
-
-				if (response != null) {
-					System.out.println("Message received"); // troubleshooting
-					if (response.mType == MessageType.USER_LOGIN
-							&& response.mStatus == MessageStatus.SUCCESS) {
-						user = (User) response.mData.get(0);
-						userName = user.getUserName();
-						System.out.println("User " + userName + " Login successful:"); // troubleshooting
-					} else {
-						System.out.println("User Login: failed - incorrect password"); // troubleshooting
+					Enumeration ee = n.getInetAddresses();
+					while (ee.hasMoreElements()) {
+						InetAddress i = (InetAddress) ee.nextElement();
+						System.out.println(i.getHostAddress()); // testing
+						if (i instanceof  java.net.Inet4Address && !i.isLoopbackAddress()) {
+							return ip = i.getHostAddress();
+						}
 					}
 				}
 			}
-
-			//System.out.println("passed login stage"); // troubleshooting
-			
-			while(true) {
-						
-	            try {
-					Thread.sleep(30000);
-		            System.out.println("Waiting for 30 seconds...");
-				} catch (InterruptedException e) {
-					System.out.println("Timer interupted");
-					e.printStackTrace();
-				}
-
-//				objOut.writeObject();
-//				objOut.flush();
-			}
-			
-
-		} catch (IOException e) {
-			System.out.println("Error while trying to read object");
+			return ip; 
+		} catch (SocketException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error while trying to find type of object");
+		}
+	}
+
+	// driver code
+	public static void main(String[] args) {
+
+		String clientIp =IPaddress();
+
+		// establish a connection by providing host and port number.
+		try (Socket socket = new Socket(clientIp, 2121)) {
+
+			// writing to server
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			// reading from server
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// object of scanner class
+			Scanner sc = new Scanner(System.in);
+			String line = null;
+
+			while (!"exit".equalsIgnoreCase(line)) {
+
+				// reading from user
+				line = sc.nextLine();
+
+				// sending the user input to server
+				out.println(line);
+				out.flush();
+
+				// displaying server reply
+				System.out.println("Server replied " + in.readLine());
+			}
+
+			// closing the scanner object
+			sc.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
